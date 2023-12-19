da2a49f3cfe3aad321788916942e5acfe22fddc1
z9999999990 <1343579921@qq.com>
2023-12-19 22:26:17 +0800

    complete

---
+++ problems/p0020.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding : utf-8
+
+"""
+Factorial digit sum
+n! means n x (n-1) x ... x 3 x 2 x 1.
+For example, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800, and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+Find the sum of the digits in the number 100!.
+"""
+
+PID = 20
+ANSWER = ''
+
+num = 100
+
+def solve() -> int :
+    i = 1
+    sum = 1
+    result = 0
+    while i <= num:
+        sum = i * sum
+        i += 1
+
+    for s in str(sum):
+        result = int(s) + result
+
+    return result
