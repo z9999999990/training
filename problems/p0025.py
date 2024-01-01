e89a7f102a76f299bf26e9c76f64ed5a8e691c20
z9999999990 <1343579921@qq.com>
2024-01-02 00:05:48 +0800

    complete

---
+++ problems/p0025.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding : utf-8
+
+"""
+Problem 25
+The Fibonacci sequence is defined by the recurrence relation:
+Fn = Fn-1 + Fn-2, where F1 = 1 and F2 = 1.
+Hence the first 12 terms will be:
+
+F1 = 1
+F2 = 1
+F3 = 2
+F4 = 3
+F5 = 5
+F6 = 8
+F7 = 13
+F8 = 21
+F9 = 34
+F10 = 55
+F11 = 89
+F12 = 144
+The 12th term, F12, is the first term to contain three digits.
+
+What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+"""
+
+PID = 25 
+ANSWER = ''
+
+def solve() ->int:
+    fn1 = 1
+    fn2 = 1
+
+    index = 2
+    while index < 1000000:
+        fn = fn1 + fn2
+        fn1 = fn2
+        fn2 = fn
+        index += 1
+        if len(str(fn)) == 1000:
+            return index 
