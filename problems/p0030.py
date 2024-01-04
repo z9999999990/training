49585c04109221894996b61a8102fde8341d5021
z9999999990 <1343579921@qq.com>
2024-01-05 00:04:57 +0800

    complete

---
+++ problems/p0030.py
@@ -0,0 +1,34 @@
+#/usr/bin/env python
+# coding : utf-8
+
+"""
+Digit Fifth Powers
+Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+    
+    1634 = 1^4 + 6^4 + 3^4 + 4^4
+    8208 = 8^4 + 2^4 + 0^4 + 8^4
+    9474 = 9^4 + 4^4 + 7^4 + 4^4
+
+As 1 = 1^4 is not a sum it is not included.
+The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+"""
+
+PID = 30
+ANSWER = 443839
+
+
+def solve() -> int:
+
+    final_sum = 0
+    max_num = pow(9, 5) * 5
+    for i in range(5, max_num+1):
+        i_list = str(i)
+        sum = 0
+        for num in i_list:
+            result = pow(int(num), 5)
+            sum += result
+        if sum == i:
+            final_sum += sum
+
+    return final_sum
